d1344d63a52a950feca387cade8ad674e770933d
Tobias1967 <tobiasschorr@methana.de>
2024-06-06 15:33:28 +0200

    Texteingabe und Löschen funktionieren jetzt

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
+import 'package:checvklist/check_list.dart';
 import 'package:flutter/material.dart';
 
-import 'package:checvklist/check_list.dart';
+import 'check_list.dart';
 
 void main() {
   runApp(const DynamicListScreen());
